d6a5284e286c210620850aeb07ca5dc8da5b3c5a
Rith <rithronlkh@gmail.com>
2018-11-01 07:57:52 +0700

    add original angularJS RestAPI pagination

---
+++ src/main/webapp/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var app = angular.module(
+  'app', 
+  [ 'ngResource',
+    'smart-table'
+    ]);
+
+app.factory('Service', function($resource) {
+    return $resource('api/person/page');
+});
+
+app.controller('TableCtrl', function ($scope, Service) {
+	
+  $scope.itemsByPage = 10;
+	
+  $scope.callServer = function(tableState) {
+    $scope.isLoading = true;
+	var pagination = tableState.pagination;
+	var start = pagination.start || 0;
+	var number = pagination.number || $scope.itemsByPage;
+	Service.get({
+      page : 1+(start/number),
+	  size : number
+	  },
+      function(pageable) {
+        $scope.pageable = pageable;
+        $scope.items = pageable.content;
+        tableState.pagination.numberOfPages = pageable.totalPages;
+        $scope.isLoading = false;
+    });
+  };
+});
